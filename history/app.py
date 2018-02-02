5ec07f1782a75714ce05ff371714bf4482e5c332
Matheus Magalhaes <m.magalhaes.ic@gmail.com>
2018-02-02 10:50:56 -0200

    Backports history for new kafka-based event interface
    This implements a kafka consumer for the data events generated
    within dojot, allowing them to be properly persisted and later
    queried as historical device data.
    
    Also, this adds proper treatment for device livecycle events,
    allowing mongo indexes to be automatically updated when needed.
    Using those indexes, history now removes old entries from database
    (through mongo's own TTL implementation)

--- history/app.py
+++ history/app.py
@@ -1,20 +1,26 @@
+#!/usr/local/bin/python
 # -*- coding: utf-8 -*-
 """
 App runner
 """
 # System imports
+import logging
+
 # Third-party imports
 import falcon
 
 # Local imports
-from history.sample.models import *
+from history import settings
+from history.api.models import DeviceHistory, STHHistory, AuthMiddleware
 
+LOGGER = logging.getLogger('history.' + __name__)
+LOGGER.addHandler(logging.StreamHandler())
+LOGGER.setLevel(logging.DEBUG)
 
 # Create falcon app
 app = falcon.API(middleware=[AuthMiddleware()])
 app.add_route('/device/{device_id}/history', DeviceHistory())
 app.add_route('/STH/v1/contextEntities/type/{device_type}/id/{device_id}/attributes/{attr}', STHHistory())
-
 
 # Useful for debugging problems in API, it works with pdb
 if __name__ == '__main__':
--- history/settings/docker.py
+++ history/settings/docker.py
@@ -3,14 +3,21 @@
 Docker settings file
 """
 # Local imports
-# Third-party imports
-import pymongo
 import os
-
-
+# Third-party imports
 # Local imports
-from base import *
+from history.settings.base import *
 
+MONGO_DB = os.environ.get('MONGO_DB', 'mongodb')
 replica_set = os.environ.get('MONGO_REPLICA_SET', None)
 
-MONGO_DB = pymongo.MongoClient('mongodb', replicaSet=replica_set)
+DATA_EXP = int(os.environ.get('DATA_EXP', '604800'))
+
+AUTH = os.environ.get('AUTH', "http://auth:5000")
+DATA_BROKER = os.environ.get('DATA_BROKER', "http://data-broker")
+KAFKA_HOST = os.environ.get('KAFKA_HOST', "kafka:9092")
+KAFKA_GROUP_ID = os.environ.get('KAFKA_GROUP_ID', "history")
+
+TENANCY_SUBJECT = os.environ.get('TENANCY_SUBJECT', "dojot.tenancy")
+DEVICE_SUBJECT = os.environ.get('DEVICE_SUBJECT', "dojot.device-manager.device")
+DATA_SUBJECT = os.environ.get('DATA_SUBJECT', "device-data")
