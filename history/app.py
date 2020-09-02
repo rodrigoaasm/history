350bf98f2fbd82bf106e1f6a20aaddfca9e3dc06
Thiago de Paula Ferreira <thiagodepaulaferreira@gmail.com>
2020-09-02 16:43:13 -0300

    revert port change

--- history/app.py
+++ history/app.py
@@ -21,5 +21,5 @@ app.add_route('/log', LoggingInterface())
 # Useful for debugging problems in API, it works with pdb
 if __name__ == '__main__':
     from wsgiref import simple_server  # NOQA
-    httpd = simple_server.make_server('127.0.0.1', 8787, app)
+    httpd = simple_server.make_server('127.0.0.1', 8000, app)
     httpd.serve_forever()
